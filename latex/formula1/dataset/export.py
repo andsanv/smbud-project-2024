e6d2dd1596904cb40267a726d85712c379826ab1
andsanv <sanvito.andrea@yahoo.com>
2025-01-05 16:31:53 +0100

    Deleted dataset dump

--- latex/formula1/dataset/export.py
+++ latex/formula1/dataset/export.py
@@ -1,10 +1,10 @@
 import json
-from latex.formula1.dataset.data_wrangling import build_championships
+from data_wrangling import build_championships
 
 def main():
     """
     Dumps the dataset in a local folder.
     """
-    json.dump(build_championships(), open("json/dataset.json", 'w'))
+    json.dump(build_championships(), open("dump.json", 'w'))
 
 main()
